f0263213d2a306d6382ac5c0721df7e550f2db83
Wang Zhongliang <zwang@rebelworks.io>
2018-01-11 11:54:37 +0800

    Remove invalid test in nicer-log

--- packages/nicer-log/test/nicer-log-test.ts
+++ packages/nicer-log/test/nicer-log-test.ts
@@ -1,7 +1,5 @@
-import hello from 'nicer-log';
-
 QUnit.module('nicer-log tests');
 
 QUnit.test('hello', assert => {
-  assert.equal(hello(), 'Hello from nicer-log');
+  assert.equal('test to be implemented', 'test to be implemented');
 });
